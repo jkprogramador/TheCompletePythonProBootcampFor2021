ae5d7640e5653b2a52197b384c5fea1e0db5c0ec
Jack Lee <jack.lee.1981@pm.me>
2020-12-15 16:45:36 -0300

    Add some useful code to selenium_scrape_pythonorg/main.py.

--- selenium_scrape_pythonorg/main.py
+++ selenium_scrape_pythonorg/main.py
@@ -5,9 +5,15 @@ CHROME_DRIVER_PATH = r"C:\Users\Jack Lee\Documents\chromedriver.exe"
 URL = "https://www.python.org/"
 
 driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
+# driver.set_window_size(width=100, height=200)
+driver.maximize_window()
 driver.get(URL)
 
 try:
+    # event_times = driver.find_elements_by_css_selector(".event-widget time")
+    # print(event_times[0].text)
+    # for time in event_times:
+
     events = driver.find_elements_by_css_selector(css_selector=".event-widget .shrubbery .menu li")
     events_dict = {}
 
